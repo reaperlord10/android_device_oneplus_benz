6f85b9ae33932be5108c274c17edf61fb8655ba2
Vivekachooz <vivekachooz@gmail.com>
2023-11-24 19:08:51 +0530

    benz: Import and adapt powerhint from redfin
    * From redfin-user-13-TQ3A.230901.001-10750268-release-keys
    
    Change-Id: I923c1adf70d035c9def0032ccf29530ce92ebe66

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -198,6 +198,8 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
--- device.mk
+++ device.mk
@@ -305,7 +305,7 @@ PRODUCT_PACKAGES += \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/crow/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 $(call soong_config_set,qtipower,tap_to_wake_node,/proc/touchpanel/double_tap_enable)
 
