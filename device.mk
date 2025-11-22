725554cc7ac09001e5163adcf417faf72a3425ee
reaperlord10 <reaperlordsgaming@gmail.com>
2025-11-22 22:07:07 +0530

    Revert "benz: Enable ELF checks for audio blobs"
    This reverts commit 9607257b35feca2b93e9bc6c74d11c5348fec9e6.

--- device.mk
+++ device.mk
@@ -44,6 +44,7 @@ PRODUCT_PACKAGES += \
     libagm_compress_plugin \
     libagm_mixer_plugin \
     libagm_pcm_plugin \
+    libats \
     libbatterylistener \
     libfmpal \
     libhfp_pal \
@@ -51,7 +52,8 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libvolumelistener \
-    sound_trigger.primary.crow
+    sound_trigger.primary.crow \
+    vendor.qti.hardware.AGMIPC@1.0-impl
 
 AUDIO_HAL_DIR := hardware/qcom-caf/sm8550/audio/primary-hal
 CONFIG_HAL_SRC_DIR := $(AUDIO_HAL_DIR)/configs/crow
@@ -243,6 +245,9 @@ PRODUCT_COPY_FILES += \
     $(AUDIO_HAL_DIR)/configs/common/codec2/media_codecs_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2_audio.xml \
     $(AUDIO_HAL_DIR)/configs/common/codec2/service/1.0/c2audio.vendor.base-arm64.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/c2audio.vendor.base-arm64.policy \
     $(AUDIO_HAL_DIR)/configs/common/codec2/service/1.0/c2audio.vendor.ext-arm64.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/c2audio.vendor.ext-arm64.policy
+
+PRODUCT_PACKAGES += \
+    libpalclient
 
 # Memtrack
 PRODUCT_PACKAGES += \
--- extract-files.py
+++ extract-files.py
@@ -9,6 +9,7 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
+    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
@@ -58,6 +59,15 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.imsrtpservice@3.0',
         'vendor.qti.imsrtpservice@3.1',
     ): lib_fixup_vendor_suffix,
+    (
+        'libar-acdb',
+        'libar-gsl',
+        'liblx-osal',
+        'libats',
+        'libagmclient',
+        'libpalclient',
+        'vendor.qti.hardware.AGMIPC@1.0-impl',
+    ): lib_fixup_remove,
 }
 
 blob_fixups: blob_fixups_user_type = {
