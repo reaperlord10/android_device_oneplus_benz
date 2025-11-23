78d2026ecb51cb5768b9a10e70fc37aef31ba0a5
reaperlord10 <reaperlordsgaming@gmail.com>
2025-11-23 07:23:51 +0530

    benz: nuke telephony jar

--- device.mk
+++ device.mk
@@ -355,9 +355,6 @@ PRODUCT_PACKAGES += \
     qti_telephony_utils_prd.xml \
     telephony-ext
 
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
