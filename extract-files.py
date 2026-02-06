a696203d77f7b0afb16c1a23462fe3a59372eca3
inferno0230 <mail@inferno0230.in>
2026-02-06 15:16:11 +0530

    benz: extract-utils: vendor.oplus.hardware.charger-V9 -> V10
    This was missed in 59df327.
    
    Change-Id: I91030e92ce9e9f42c0a176c7df0bd32ba621feb1
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- extract-files.py
+++ extract-files.py
@@ -82,7 +82,7 @@ blob_fixups: blob_fixups_user_type = {
         .regex_replace('SystemCamera =  0;  0;  1;  1;', 'SystemCamera =  0;  0;  0;  0;'),
     'odm/bin/hw/vendor.oplus.hardware.biometrics.fingerprint@2.1-service_uff': blob_fixup()
         .add_needed('libshims_aidl_fingerprint_v2.oplus.so'),
-    'odm/bin/hw/vendor.oplus.hardware.charger-V9-service': blob_fixup()
+    'odm/bin/hw/vendor.oplus.hardware.charger-V10-service': blob_fixup()
         .add_needed('libbase_shim.so'),
     'odm/etc/init/init.network.rc': blob_fixup()
         .regex_replace(r'/\* (Huo\.Chen@SYSTEM\.RF, 2024/09/06, Add for ICC) \*/', r'# \1'),
