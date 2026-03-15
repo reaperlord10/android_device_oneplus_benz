2c69c807375453c5f22a427cb9debd03cf1c690e
pjgowtham <pjgowtham@gmail.com>
2026-03-15 15:24:23 +0530

    benz: Build composer with support for userspace dim layer
    Change-Id: Iea2c5f5d34ec6365c31ae9d3c7c36833983baa17
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- device.mk
+++ device.mk
@@ -162,6 +162,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
+$(call soong_config_set_bool,qtidisplay,oplus_udfps,true)
 
 # GPS
 PRODUCT_COPY_FILES += \
