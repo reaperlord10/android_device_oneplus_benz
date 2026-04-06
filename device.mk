296d82a94daf32397fb90ab3a6181be4cf66e454
inferno0230 <mail@inferno0230.in>
2026-04-06 19:22:04 +0530

    benz: Enable Oplus awinic support
    Change-Id: I63ad3fcf9c7a2a828ec053b5ba31ce0980dce18d
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- device.mk
+++ device.mk
@@ -426,6 +426,7 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set,OPLUS_LINEAGE_VIBRATOR_HAL,INCLUDE_DIR,$(LOCAL_PATH)/vibrator)
 $(call soong_config_set_bool,OPLUS_LINEAGE_VIBRATOR_HAL,OVERRIDE_EFFECTS,true)
+$(call soong_config_set_bool,OPLUS_LINEAGE_VIBRATOR_HAL,USES_OPLUS_AWINIC,true)
 
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
